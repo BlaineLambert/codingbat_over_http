252736f2e57bf8376072609d0617f80da2afd5e0
BlaineLambert <flambert24@basecampcodingacademy.org>
2023-11-09 15:05:59 -0600

    done

--- app/views.py
+++ app/views.py
@@ -4,26 +4,26 @@ from django.http.request import HttpRequest
 
 # Create your views here.
 
-def near_hundred(request: HttpRequest, num: HttpRequest) -> HttpResponse:
+def near_hundred(request: HttpRequest, num: int) -> HttpResponse:
     if abs(num - 100) <= 10 or abs(num - 200) <= 10:
         return HttpResponse(True)
     else:
         return HttpResponse(False)
     
-def string(request: HttpRequest, char: HttpRequest) -> HttpResponse:
+def string(request: HttpRequest, char: str) -> HttpResponse:
     result = ""
     for i in range(len(char)):
         result += char[:i + 1]
 
     return HttpResponse(result)
 
-def counter(request: HttpRequest, list: HttpRequest) -> HttpResponse:
+def counter(request: HttpRequest, list: str) -> HttpResponse:
     cat_count = list.count("cat")
     dog_count = list.count("dog")
 
     return HttpResponse(cat_count == dog_count)
 
-def numcount(request: HttpRequest, num1: HttpRequest, num2: HttpRequest, num3: HttpRequest) -> HttpResponse:
+def numcount(request: HttpRequest, num1: int, num2: int, num3: int) -> HttpResponse:
     if num1 == num2 and num1 == num3 and num2 == num3:
         return HttpResponse(0)
     if num1 == num2 and num1 != num3 or num1 == num2 and num2 != num3:
