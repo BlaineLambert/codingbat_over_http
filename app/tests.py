7001462275a891a68dc0e075447421ccad974f3b
BlaineLambert <flambert24@basecampcodingacademy.org>
2023-11-27 09:47:53 -0600

    done

--- app/tests.py
+++ app/tests.py
@@ -1,56 +1,57 @@
-
 # Create your tests here.
 from django.test import SimpleTestCase
 
 
 class Testnear_hundred(SimpleTestCase):
     def test_89(self):
-        response = self.client.get("/hundred/89")
+        response = self.client.get("/warmup-1/near-hundred/89/")
         self.assertContains(response, False)
 
     def test_100(self):
-        response = self.client.get("/hundred/93")
+        response = self.client.get("/warmup-1/near-hundred/93/")
         self.assertContains(response, True)
 
     def test_90(self):
-        response = self.client.get("/hundred/90")
+        response = self.client.get("/warmup-1/near-hundred/90/")
         self.assertContains(response, True)
 
 class Testcat_dog(SimpleTestCase):
     def test_cadog(self):
-        response = self.client.get("/catdogcheck/1cat1cadodog")
-        self.assertContains(response, False)
-    
+        response = self.client.get("/string-2/cat-dog/1cat1cadodog/")
+        self.assertContains(response, True)
+
     def test_catdo(self):
-        response = self.client.get("/catdogcheck/catcat")
+        response = self.client.get("/string-2/cat-dog/catcat/")
         self.assertContains(response, False)
 
     def test_catdog(self):
-        response = self.client.get("/catdogcheck/catdog")
+        response = self.client.get("/string-2/cat-dog/catdog/")
         self.assertContains(response, True)
+
 
 class Testnumbers(SimpleTestCase):
     def test_121(self):
-        response = self.client.get("/numbers/3/2/3")
+        response = self.client.get("/logic-2/lone-sum/3/2/3/")
         self.assertContains(response, 2)
-    
+
     def test_111(self):
-        response = self.client.get("/numbers/3/3/3")
+        response = self.client.get("/logic-2/lone-sum/1/1/1/")
         self.assertContains(response, 0)
 
     def test_123(self):
-        response = self.client.get("/numbers/1/2/3")
+        response = self.client.get("/logic-2/lone-sum/1/2/3/")
         self.assertContains(response, 6)
+
 
 class Teststring(SimpleTestCase):
     def test_Code(self):
-        response = self.client.get("/string/Code")
+        response = self.client.get("/warmup-2/string-splosion/Code/")
         self.assertContains(response, "CCoCodCode")
 
     def test_ab(self):
-        response = self.client.get("/string/ab")
+        response = self.client.get("/warmup-2/string-splosion/ab/")
         self.assertContains(response, "aab")
 
     def test_hi(self):
-        response = self.client.get("/string/abc")
+        response = self.client.get("/warmup-2/string-splosion/abc/")
         self.assertContains(response, "aababc")
